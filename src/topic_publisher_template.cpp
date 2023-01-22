b3d7d5a92baa7833baff0e3f4b1b8d39c2d69571
Daiki Murakami <dai.murakami98@gmail.com>
2023-01-22 15:35:21 +0900

    fix : namespace

--- src/topic_publisher_template.cpp
+++ src/topic_publisher_template.cpp
@@ -8,7 +8,7 @@
 
 using namespace std::chrono_literals;
 
-namespace ros_template_programs {
+namespace ros2_template_programs {
     class TopicPublisher  : public rclcpp::Node {
         private:
             rclcpp::TimerBase::SharedPtr timer_;
@@ -21,12 +21,12 @@ namespace ros_template_programs {
     };
 }
 
-void ros_template_programs::TopicPublisher::callbackTimer() {
+void ros2_template_programs::TopicPublisher::callbackTimer() {
     RCLCPP_INFO(this->get_logger(), "Message Published: '%s'", msg_.data.c_str());
     pub_msg_ ->publish(msg_);
 }
 
-ros_template_programs::TopicPublisher::TopicPublisher() : Node("topic_publisher_template") {
+ros2_template_programs::TopicPublisher::TopicPublisher() : Node("topic_publisher_template") {
     pub_msg_  = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(500ms, std::bind(&TopicPublisher::callbackTimer, this));
     declare_parameter( "message", "Hello, World!");
@@ -35,7 +35,7 @@ ros_template_programs::TopicPublisher::TopicPublisher() : Node("topic_publisher_
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ros_template_programs::TopicPublisher>());
+    rclcpp::spin(std::make_shared<ros2_template_programs::TopicPublisher>());
     rclcpp::shutdown();
     return 0;
 }
--- src/topic_subscriber_template.cpp
+++ src/topic_subscriber_template.cpp
@@ -5,7 +5,7 @@
 
 using std::placeholders::_1;
 
-namespace ros_template_programs {
+namespace ros2_template_programs {
     class TopicSubscriber : public rclcpp::Node {
         private:
             rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_msg_;
@@ -15,17 +15,17 @@ namespace ros_template_programs {
     };
 }
 
-void ros_template_programs::TopicSubscriber::callbackMessage(const std_msgs::msg::String & msg) const {
+void ros2_template_programs::TopicSubscriber::callbackMessage(const std_msgs::msg::String & msg) const {
     RCLCPP_INFO(this->get_logger(), "Message Subscribed: '%s'", msg.data.c_str());
 }
 
-ros_template_programs::TopicSubscriber::TopicSubscriber() : Node("topic_subscriber_template") {
+ros2_template_programs::TopicSubscriber::TopicSubscriber() : Node("topic_subscriber_template") {
     sub_msg_ = this->create_subscription<std_msgs::msg::String>( "topic", 10, std::bind(&TopicSubscriber::callbackMessage, this, _1));
 }
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ros_template_programs::TopicSubscriber>());
+    rclcpp::spin(std::make_shared<ros2_template_programs::TopicSubscriber>());
     rclcpp::shutdown();
     return 0;
 }
